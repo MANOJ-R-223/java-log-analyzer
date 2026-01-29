7ed0c1eb315cb833467cb4ac4240146e9324336e
manoj <deadlyduo223@gmail.com>
2026-01-29 15:42:03 +0530

    Refactor log analysis logic into methods

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -6,7 +6,19 @@ public class LogAnalyzer {
 
     public static void main(String[] args) {
 
-        String logFilePath = "logs/sample.log";
+        if (args.length == 0) {
+            System.out.println("Run with Filepath  Example: java LogAnalyzer <log-file-path>");
+            return;
+        }
+
+        String logFilePath = args[0];
+
+        analyzeLogFile(logFilePath);
+        
+    }
+
+    private static void analyzeLogFile(String logFilePath) {
+
         int totalLines = 0;
         int errorCount = 0;
         int failedLoginCount = 0;
@@ -27,14 +39,23 @@ public class LogAnalyzer {
                 }
             }
 
-            System.out.println("Log Analysis Summary");
-            System.out.println();
-            System.out.println("Total log entries: " + totalLines);
-            System.out.println("ERROR entries: " + errorCount);
-            System.out.println("Failed login attempts: " + failedLoginCount);
+            printSummary(totalLines, errorCount, failedLoginCount);
 
         } catch (IOException e) {
             System.out.println("Error reading log file: " + e.getMessage());
         }
+
     }
+
+    private static void printSummary(int total, int errors, int failedLogins){
+
+        System.out.println("Log Analysis Summary");
+            System.out.println();
+            System.out.println("Total log entries: " + total);
+            System.out.println("ERROR entries: " + errors);
+            System.out.println("Failed login attempts: " + failedLogins);
+
+    }
+        
+    
 }
